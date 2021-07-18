b59aff991e63cbac1fbc1b16724efc7629052291
Satyam Soni <satyamsoni@hotmail.co.uk>
2021-07-18 18:31:55 +0530

    Fixed docker compose

--- server/djangoNext/settings.py
+++ server/djangoNext/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7x*t1zxtj4c^$4=l@mi2gmyrmvip!gtlm02-2bgm!1t#y4pod=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
